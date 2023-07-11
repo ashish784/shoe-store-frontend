79e071ecb9c4362890698f87cf2b5589d04bfce9
ashish784 <mauryaashish784@gmail.com>
2023-07-11 16:04:38 +0530

    optional chaining added and git ignore updated

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -154,25 +154,7 @@ const ProductDetails = ({product, products}) => {
                 {/* WISHLIST START  */}
                 <button className="w-full py-4 rounded-full border border-black text-lg
                 font-medium transition-transform active:scale-95 flex items-center 
-                justify-center gap-2 hover:opacity-75 mb-10"
-                onClick={()=> {
-                    if(!selectedSize) {
-                        setShowError(true);
-                        document.getElementById("sizeGrid").scrollIntoView({
-                            block: "center",
-                            behavior: "smooth"
-                        })
-                    } else {
-                        dispatch(addToWishlist({
-                            ...product?.data?.[0],
-                            selectedSize,
-                            oneQuantityPrice: p.price,
-                        })
-                       );
-                       notify();
-                    }
-                    
-                }}>
+                justify-center gap-2 hover:opacity-75 mb-10">
                     WISHLIST
                     <IoMdHeartEmpty size={20} />
                 </button>
